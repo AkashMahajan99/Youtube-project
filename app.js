b563027b9db186fa4bb60c83b9b1f9ac91b84def
Akash Mahajan <akkim3899@gmail.com>
2023-03-12 15:10:54 +0530

    videocard created

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './src/components/Header';
+import Sidebar from './src/components/Sidebar';
+import Videocontainer from './src/components/Videocontainer';
 
 const AppLayout = () => {
     return (
       <div className=' '>
-    
+    <div>
        <Header/>
+      </div>
+      <div className='flex flex-row h-[calc(100vh-50px)]'>
+        <Sidebar/>
+      <Videocontainer/>
+      </div>
       </div>
     );
   };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import Hamlogo from "./assets/Hamburger.svg"
+import Hamlogo from "./assets/Hamburger.svg";
+import Ytlogo from './assets/Logo_of_YouTube_(2015-2017).svg';
+import Searchbar from './Searchbar';
 const Header = () => {
   return (
-    <div className='  flex '>
-       <div className=''>
-       <img src={Hamlogo} alt="" />
+    <div className='flex w-100 h-[50px] p-2 justify-between shadow-md '>
+       <div className=' flex'>
+       <img className='w-8 h-8 ml-3' src={Hamlogo} alt="hamlogo" />
+       <img className="mt-1 h-6 ml-3" src={Ytlogo} alt="video-icon" />
+
          </div>
-       <div className=' '></div>
-       <div className=' '> profile</div>
+       <Searchbar/>
+       <div className='mx-2'>
+                <img className="w-8 h-8 text-black-900" src={Hamlogo} alt="profile-icon" />
+            </div>
     </div>
   )
 }

356e04dc79141366b40afe1334f6bbab792f642a
Akash Mahajan <akkim3899@gmail.com>
2023-03-12 14:36:46 +0530

    Untracked files: 	src/components/Searchbar.js 	src/components/Sidebar.js 	src/components/Videocard.js 	src/components/Videocontainer.js 	src/components/assets/Logo_of_YouTube_(2015-2017).svg

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import Header from './src/components/Header';
 
 const AppLayout = () => {
     return (
-      <div className=' flex h-20 bg-slate-400 border-solid border mt-4 '>
+      <div className=' '>
     
        <Header/>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import Hamburger from './assets/hamburger.svg';
-import Ytlogo from "./assets/youtubelogo.svg";
+import Hamlogo from "./assets/Hamburger.svg"
 const Header = () => {
   return (
-    <div className=' p-1 flex justify-between'>
-       <div className=' text-emerald-500 border-l-black bg-cyan-400 mx-5'>
-        <img src={Hamburger} alt="hamburgericon" />
-        <img src={Ytlogo} alt="" />
+    <div className='  flex '>
+       <div className=''>
+       <img src={Hamlogo} alt="" />
          </div>
-       <div className=' mx-10  bg-red-700'><input type="text" /></div>
-       <div className=' bg-yellow-400'> profile</div>
+       <div className=' '></div>
+       <div className=' '> profile</div>
     </div>
   )
 }

a1d4a9949c2fb60cba9dd2413c4c819aefac0db8
Akash Mahajan <akkim3899@gmail.com>
2023-04-09 01:42:52 +0530

    ytlogo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Hamburger from './assets/hamburger.svg'
+import Hamburger from './assets/hamburger.svg';
+import Ytlogo from "./assets/youtubelogo.svg";
 const Header = () => {
   return (
     <div className=' p-1 flex justify-between'>
        <div className=' text-emerald-500 border-l-black bg-cyan-400 mx-5'>
         <img src={Hamburger} alt="hamburgericon" />
+        <img src={Ytlogo} alt="" />
          </div>
        <div className=' mx-10  bg-red-700'><input type="text" /></div>
        <div className=' bg-yellow-400'> profile</div>

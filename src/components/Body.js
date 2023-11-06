97c1db20c77468efe22dcabb94aeb0b56b4b2c4f
Akash Mahajan <akkim3899@gmail.com>
2023-11-07 01:21:56 +0530

    cleanup

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Sidebar from './Sidebar';
-
-import Maincontainer from './Maincontainer';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 const Body = () => {
